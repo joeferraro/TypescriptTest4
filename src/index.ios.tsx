3d9eec9a2a696941fc778413243a29f214f969d1
David Helmer <dhelmer@mavens.com>
2016-10-26 09:16:30 -0400

    initial commit ts 1.8

---
+++ src/index.ios.tsx
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class TypescriptTest3 extends Component<Props,State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!!!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+interface Props { 
+}
+
+interface State {
+}
+
+interface Style { 
+  container: React.ViewStyle,
+  welcome: React.TextStyle,
+  instructions: any
+}
+
+
+const styles = StyleSheet.create<Style>({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TypescriptTest3', () => TypescriptTest3);
